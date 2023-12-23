27bb2ccfe7fe6b673ae3105f6211819230a79ce2
leavez <gaojiji@gmail.com>
2023-12-24 02:00:31 +0800

    add config.h manually

---
+++ config.h
@@ -0,0 +1,47 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define if libfuse cache_readdir is available */
+#define HAVE_FUSE_CACHE_READDIR 0
+
+/* Define if libfuse has copy_file_range is available */
+#define HAVE_FUSE_COPY_FILE_RANGE 0
+
+// ---------------------
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if the system has the type `_Bool'. */
+#define HAVE__BOOL 1
+
+/* Use the same hashing function as GNU diff */
+#define USE_DIFF_HASH 1
+
+// ---------------------
+
+/* Name of package */
+#define PACKAGE "fuse-overlayfs"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "giuseppe@scrivano.org"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "fuse-overlayfs"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "fuse-overlayfs 1.13-dev"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "fuse-overlayfs"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.13-dev"
+
+/* Version number of package */
+#define VERSION "1.13-dev"
